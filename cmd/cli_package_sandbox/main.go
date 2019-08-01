e81bc8f7f2d1f492dc2175dcad099a58372fbfff
tokunagawataru <tokunaga.wataru31@gmail.com>
2019-08-02 02:00:39 +0900

    add outpus

--- cmd/cli_package_sandbox/main.go
+++ cmd/cli_package_sandbox/main.go
@@ -9,6 +9,23 @@ import (
 )
 
 func main() {
+	//output:
+	//
+	//NAME:
+	//	cli_package_sandbox - cli package test
+	//
+	//USAGE:
+	//	main [global options] command [command options] [arguments...]
+	//
+	//	VERSION:
+	//	0.0.1
+	//
+	//	COMMANDS:
+	//	help, h  Shows a list of commands or help for one command
+	//
+	//	GLOBAL OPTIONS:
+	//	--help, -h     show help
+	//	--version, -v  print the version
 	app := cli.NewApp()
 	app.Name = "cli_package_sandbox"
 	app.Usage = "cli package test"
